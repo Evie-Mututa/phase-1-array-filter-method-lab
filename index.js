e790641dd979c395eaa9e4cb2dc6954a833f395d
Evie-Mututa <evemututa29@gmail.com>
2023-10-12 09:38:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(drivers, searchString) {
+    // Convert the search string to lowercase for case insensitivity
+    const searchLower = searchString.toLowerCase();
+  
+    // Use the filter method to find matching driver names
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === searchLower);
+  
+    return matchingDrivers;
+  }
+
+  function fuzzyMatch(drivers, searchString) {
+    // Use the filter method to find drivers whose names start with searchString
+    const matchingDrivers = drivers.filter(driver => driver.startsWith(searchString));
+  
+    return matchingDrivers;
+  }
+
+  function matchName(drivers, searchString) {
+    // Use the filter method to find driver objects with matching names
+    const matchingDrivers = drivers.filter(driver => driver.name === searchString);
+  
+    return matchingDrivers;
+  }
+  
